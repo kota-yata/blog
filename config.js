4b996eb42ecefea99321cb094d4712d022f4c757
kota-yata <kota@yatagai.com>
2021-01-08 21:02:26 +0900

    create gatsby blog

---
+++ config.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = {
+  url: 'https://blog.kota-yata.com',
+  pathPrefix: '/',
+  title: 'kota-yata',
+  subtitle: '17yo / フロントエンド開発者',
+  copyright: '© 2021 Kota Yatagai',
+  disqusShortname: '',
+  postsPerPage: 10,
+  googleAnalyticsId: 'UA-73379983-2',
+  useKatex: true,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    }
+  ],
+  author: {
+    name: 'Kota Yatagai',
+    photo: '/photo.jpg',
+    bio: '17yo / フロントエンド開発者',
+    contacts: {
+      email: 'kota@yatagai.com',
+      facebook: '',
+      instagram: '',
+      telegram: '',
+      twitter: 'kota_yata',
+      github: 'kota-yata',
+      rss: '',
+      vkontakte: '',
+      linkedin: '',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+      medium: '',
+    }
+  }
+};
