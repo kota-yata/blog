950c9a9d90c4a24b0e91473b8e49ab89c4524a6b
kota-yata <kota@yatagai.com>
2023-02-28 16:23:02 +0900

    modify svelte.config.js

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,7 @@
+<script context="module">
+  export const prerender = true;
+</script>
+
 <script lang="ts">
   import '../styles/app.scss';
   import '../styles/fonts.scss';
--- svelte.config.js
+++ svelte.config.js
@@ -13,8 +13,8 @@ const config = {
     adapter: adapter({
       pages: 'build',
       assets: 'build',
-      fallback: null
-    })
+    }),
+    trailingSlash: 'always'
   },
 };
 
