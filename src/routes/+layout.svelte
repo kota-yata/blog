c01071dbc774b3cb2d66e4c5306f5e46cc92b25a
kota-yata <kota@yatagai.com>
2023-02-28 16:28:55 +0900

    traling slash

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,3 @@
-<script context="module">
-  export const prerender = true;
-</script>
-
 <script lang="ts">
   import '../styles/app.scss';
   import '../styles/fonts.scss';
--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,6 @@ const config = {
       pages: 'build',
       assets: 'build',
     }),
-    trailingSlash: 'always'
   },
 };
 
